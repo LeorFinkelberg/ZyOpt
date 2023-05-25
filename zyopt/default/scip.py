bf711eb5c9c8dec73d8769b7ea0bf10c29059676
Alexander Podvoyskiy <alexander.podvoyskiy@idp.zyfra.com>
2023-05-25 17:19:39 +0300

    chore: add new featuer

--- zyopt/default/scip.py
+++ zyopt/default/scip.py
@@ -1,7 +1,10 @@
+import sys
 import typing as t
 
+import pandas as pd
 import pyscipopt
 from pathlib2 import Path
+from tqdm import tqdm
 
 from zyopt._base_model import Model
 from zyopt.common.constants import *
@@ -19,9 +22,10 @@ class ScipModel(Model):
     def __init__(
         self,
         *,
-        mode: str,
-        path_to_problem: str,
+        solver_mode: str,
         path_to_params: str,
+        path_to_problem: t.Optional[str] = None,
+        model: t.Optional[pyscipopt.scip.Model] = None,
     ):
         PYSCIPOPT_CURRENT_VERSION = tuple(map(int, pyscipopt.__version__.split(".")))
         if PYSCIPOPT_CURRENT_VERSION < PYSCIPOPT_APACHE_2_0_LICENSE_VERSION:
@@ -30,22 +34,35 @@ class ScipModel(Model):
                 "which is only available under ZIB ACADEMIC LICENSE. "
                 "See https://www.scipopt.org/academic.txt"
             )
-        self.mode = mode
-        self.path_to_problem = Path(path_to_problem)
+        self.solver_mode = solver_mode
         self.path_to_params = Path(path_to_params)
 
-        self._model = pyscipopt.Model()
-        logger.info(f"Reading problem: {self.path_to_problem}")
-        self._model.readProblem(self.path_to_problem)
+        if model is not None:
+            self._model = model
+        else:
+            try:
+                self.path_to_problem = Path(path_to_problem)
+            except TypeError as err:
+                raise
+
+            self._model = pyscipopt.Model()
+            logger.info(f"Reading problem: {self.path_to_problem}")
+            self._model.readProblem(self.path_to_problem)
+
         logger.info(f"Reading params: {self.path_to_params}")
         self._model.readParams(self.path_to_params)
+
+        self.all_vars_: t.Iterable[pyscipopt.scip.Variable] = self._model.getVars()
+        self.all_var_names_: t.Iterable[str] = [var.name for var in self.all_vars_]
+        self.all_vars = pd.Series(self.all_vars_, index=self.all_var_names_)
 
     def optimize(self):
         """
         Optimize the problem
         """
-        if self.mode == SOLVER_MODE_RELAX:
-            _all_vars: t.List[pyscipopt.scip.Variable] = self._model.getVars()
+        _all_vars: t.Iterable[pyscipopt.scip.Variable] = self._model.getVars()
+
+        if self.solver_mode == SOLVER_MODE_RELAX:
             _bin_int_vars: t.List[pyscipopt.scip.Variable] = [
                 var for var in _all_vars if var.vtype() != VAR_TYPE_CONTINUOUS
             ]
@@ -54,18 +71,43 @@ class ScipModel(Model):
             for var in _bin_int_vars:
                 self._model.chgVarType(var, VAR_TYPE_CONTINUOUS)
 
-        logger.info(f"Running SCIP in {self.mode.upper()} mode ...")
+        logger.info(f"Running SCIP in {self.solver_mode.upper()} mode ...")
         self._model.optimize()
 
-    def get_best_sol(self) -> dict:
+    def convert_best_sol_to_dict(self) -> dict:
         """
-        Get the best SCIP sol in dict format
+        Converts SCIP solution to dict
         """
+        sol = self._model.getBestSol()
+        return {var.name: self._model.getSolVal(sol, var) for var in self.all_vars_}
 
-        sol: pyscipopt.scip.Solution = self._model.getBestSol()
-        vars_: t.Iterable[pyscipopt.scip.Variable] = self._model.getVars()
+    def get_sols(self) -> t.Iterable[pyscipopt.scip.Solution]:
+        """
+        Retrieve list of all feasible primal solutions stored in the solution storage
+        """
+        return self._model.getSols()
 
-        return {var.name: self._model.getSolVal(sol, var) for var in vars_}
+    def get_best_sol(self) -> t.Optional[dict]:
+        """
+        Get the best SCIP solution
+        """
+        status: str = self.get_status()
+
+        if status == SCIP_STATUS_IFEASIBLE:
+            return None
+        elif status in (
+            SCIP_STATUS_OPTIMAL,
+            SCIP_STATUS_GAPLIMIT,
+            SCIP_STATUS_TIMELIMIT,
+            SCIP_STATUS_USERINTERRUPT,
+        ):
+            pool_sols: t.Iterable[pyscipopt.scip.Solution] = self.get_sols()
+
+            if pool_sols:
+                return self.convert_best_sol_to_dict()
+            else:
+                logger.info("Process is interrupted ...")
+                sys.exit(-1)
 
     def get_status(self) -> str:
         """
@@ -79,6 +121,12 @@ class ScipModel(Model):
         to their values
         """
         return self._model.getParams()
+
+    def get_vars(self) -> t.Iterable[pyscipopt.scip.Variable]:
+        """
+        Retrive all variables
+        """
+        return self._model.getVars()
 
     def write_best_sol(
         self,
@@ -100,3 +148,14 @@ class ScipModel(Model):
             logger.error(err)
         else:
             logger.info(f"Sol-file '{path_to_sol}' was successfully written")
+
+    def fix_vars(self, base_for_fix: pd.Series, var_names: t.List[str]):
+        """
+        Fix vars in base
+        """
+        for var in tqdm(self.all_vars.loc[var_names]):
+            var_name: str = var.name
+            value: float = base_for_fix.loc[var_name]
+            self._model.fixVar(var, value)
+
+        return self._model
--- zyopt/heuristics/basic/fbivrs.py
+++ zyopt/heuristics/basic/fbivrs.py
@@ -1,5 +1,18 @@
+import sys
+import typing as t
+from copy import copy
+
+import numpy as np
+import pandas as pd
+import pyscipopt
+from pathlib2 import Path
+from tqdm import tqdm
+
 from zyopt._base_model import Model
+from zyopt.common.constants import *
 from zyopt.common.logger import make_logger
+from zyopt.config import DECIMALS, RANDOM_SEED
+from zyopt.default.scip import ScipModel
 
 logger = make_logger(__file__)
 
@@ -11,16 +24,167 @@ class FixBinaryIntegerVarsInRelaxSolModel(Model):
 
     def __init__(
         self,
-        mode: str,
         path_to_problem: str,
         path_to_relax_params: str,
         path_to_milp_params: str,
-        binary: bool = True,
-        integer: bool = True,
+        path_to_sol: str,
+        use_binary: bool = True,
+        use_integer: bool = True,
         fix_vars_lower_threshold: float = 0.0,
         fix_vars_upper_threshold: float = 0.0,
-        rounds: int = 100,
+        perturb_rounds: int = 100,
         distr_lower_threshold: float = -0.9,
-        dist_upper_threshold: float = 0.1,
+        distr_upper_threshold: float = 0.1,
     ):
-        pass
+        self.path_to_problem = path_to_problem
+        self.path_to_relax_params = path_to_relax_params
+        self.path_to_milp_params = path_to_milp_params
+        self.path_to_sol = path_to_sol
+        self.use_binary = use_binary
+        self.use_integer = use_integer
+        self.fix_vars_lower_threshold = fix_vars_lower_threshold
+        self.fix_vars_upper_threshold = fix_vars_upper_threshold
+        self.perturb_rounds = perturb_rounds
+        self.distr_lower_threshold = distr_lower_threshold
+        self.distr_upper_threshold = distr_upper_threshold
+
+    def optimize(self):
+        self._relax_model = ScipModel(
+            solver_mode=SOLVER_MODE_RELAX,
+            path_to_problem=self.path_to_problem,
+            path_to_params=self.path_to_relax_params,
+        )
+        self._relax_model.optimize()
+        sol: t.Optional[dict] = self._relax_model.get_best_sol()
+
+        self._milp_model = ScipModel(
+            solver_mode=SOLVER_MODE_MILP,
+            path_to_problem=self.path_to_problem,
+            path_to_params=self.path_to_milp_params,
+        )
+
+        if sol is not None:
+            _relax_sol = pd.Series(sol)
+            relax_sol = _relax_sol.loc[
+                self._get_var_names(self.use_binary, VAR_TYPE_BINARY)
+                + self._get_var_names(self.use_integer, VAR_TYPE_INTEGER)
+            ]
+            relax_sol = np.round(relax_sol, decimals=DECIMALS)
+
+            mask = self._make_mask(
+                relax_sol, self.fix_vars_lower_threshold, self.fix_vars_upper_threshold
+            )
+            var_names: t.List[str] = relax_sol.loc[mask].index.to_list()
+
+            _modif_model = self._milp_model.fix_vars(relax_sol, var_names)
+            _milp_model = ScipModel(
+                solver_mode=SOLVER_MODE_MILP,
+                path_to_params=self.path_to_milp_params,
+                model=_modif_model,
+            )
+            _milp_model.optimize()
+            status = _milp_model.get_status()
+
+            if status == SCIP_STATUS_IFEASIBLE:
+                np.random.seed(RANDOM_SEED)
+
+                for round_ in range(self.perturb_rounds):
+                    logger.info(
+                        f"Running SCIP with perturbation relax sol: {round_ + 1} / {self.perturb_rounds}"
+                    )
+
+                    noise = np.random.uniform(
+                        low=self.distr_lower_threshold,
+                        high=self.distr_upper_threshold,
+                        size=relax_sol.shape[0],
+                    )
+                    relax_sol_with_noise = relax_sol + noise
+                    relax_sol_with_noise = pd.Series(
+                        np.where(relax_sol_with_noise < 0.0, 0.0, relax_sol_with_noise),
+                        index=relax_sol.index,
+                    )
+                    mask = self._make_mask(
+                        relax_sol_with_noise,
+                        self.fix_vars_lower_threshold,
+                        self.fix_vars_upper_threshold,
+                    )
+                    var_names: t.List[str] = relax_sol_with_noise.loc[mask].index.to_list()
+
+                    _milp_model = ScipModel(
+                        solver_mode=SOLVER_MODE_MILP,
+                        path_to_problem=self.path_to_problem,
+                        path_to_params=self.path_to_milp_params,
+                    )
+                    _modif_model = _milp_model.fix_vars(relax_sol_with_noise, var_names)
+                    _milp_model = ScipModel(
+                        solver_mode=SOLVER_MODE_MILP,
+                        path_to_params=self.path_to_milp_params,
+                        model=_modif_model,
+                    )
+                    _milp_model.optimize()
+                    status = _milp_model.get_status()
+
+                    if status == SCIP_STATUS_IFEASIBLE:
+                        continue
+                    else:
+                        _milp_model.write_best_sol(self.path_to_sol)
+                        break
+            else:
+                _milp_model.write_best_sol(self.path_to_sol)
+
+        else:
+            logger.info("Infeasible RELAX solution. Check settings")
+
+    @staticmethod
+    def _get_vars_by_type(
+        vars_: t.Iterable[pyscipopt.scip.Variable],
+        var_type: str,
+    ) -> t.Iterable[pyscipopt.scip.Variable]:
+        """
+        Gets vars by type
+        """
+        return [var for var in vars_ if var.vtype() == var_type]
+
+    def _get_var_names(self, use_vars: bool, var_type: str) -> t.List[str]:
+        """
+        Get var names
+        """
+        vars_: t.List[pyscipopt.scip.Variable] = (
+            self._get_vars_by_type(
+                vars_=self._milp_model.all_vars_,
+                var_type=var_type,
+            )
+            if use_vars
+            else []
+        )
+
+        return [var.name for var in vars_]
+
+    @staticmethod
+    def _make_mask(base_for_fix: pd.Series, lower_threshold: float, upper_threshold: float) -> pd.Series:
+        """
+        Makes bool mask
+        """
+        if lower_threshold == upper_threshold:
+            # lower_threshold = upper_threshold = 0 or lower_threshold = upper_threshold = 1
+            fix_value = lower_threshold
+            mask = base_for_fix == fix_value
+        elif upper_threshold is None:
+            # Example: lower_threshold = 0.05 and upper_threshold = NULL
+            base_for_fix[base_for_fix <= lower_threshold] = 0.0
+            mask = base_for_fix == 0.0
+        elif lower_threshold is None:
+            # Example: upper_threshold = 0.95 and lower_threshold = NULL
+            base_for_fix[base_for_fix >= upper_threshold] = 1.0
+            mask = base_for_fix == 1.0
+        else:
+            # Example: lower_threshold = 0.05 and upper_threshold = 0.95
+            base_for_fix[base_for_fix <= lower_threshold] = 0.0
+            mask_for_zero = base_for_fix == 0.0
+
+            base_for_fix[base_for_fix >= upper_threshold] = 1.0
+            mask_for_one = base_for_fix == 1.0
+
+            mask = mask_for_zero | mask_for_one
+
+        return mask
